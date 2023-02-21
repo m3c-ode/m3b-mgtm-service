eed285119f506bd8aed65b4525ea56ac63884179
maxime m3.code <maxime.marechalmccoy@gmail.com>
2023-02-21 12:22:39 -0800

    allowCors middleware

--- src/pages/api/beers/[[...params]].ts
+++ src/pages/api/beers/[[...params]].ts
@@ -6,6 +6,25 @@ import { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "../../../../lib/mongodb";
 import type { BeerData, NewBeerData } from "../../../types/beers";
 
+// base default url here?
+
+const allowCors = (fn: any) => async (req: any, res: any) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    return await fn(req, res);
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const client = await clientPromise;
     // creates and use a db called "test"
@@ -86,3 +105,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             break;
     }
 }
+
+module.exports = allowCors(handler);
