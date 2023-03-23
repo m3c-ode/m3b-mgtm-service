1759080cd7ad180755706158f724dc302ec79176
maxime m3.code <maxime.marechalmccoy@gmail.com>
2023-03-23 10:13:41 -0700

    fixed cred in link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default function Document() {
                 <script
                     src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_PLACES_API_KEY}&libraries=places,maps`}
                 />
-                {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossOrigin="anonymous" /> */}
             </Head>
             <body>
                 <Main />
